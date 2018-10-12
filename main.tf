52ac7e0c8f64f860b4eaa196d884473f0c39dc7f
bhvkrishna90 <krishna_bhimanavarjula@epam.com>
2018-10-12 11:34:23 +0530

    uodated main and variables file

--- main.tf
+++ main.tf
@@ -3,5 +3,9 @@ provider "azurerm" {
 }
 
 terraform {
-    backend "azurerm" {}
+    backend "azurerm" {
+        storage_account_name = "abcd1234"
+        container_name       = "tfstate"
+        key                  = "prod.terraform.tfstate"
+    }
 }
--- variables.tf
+++ variables.tf
@@ -2,25 +2,37 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "agent_count" {
+    description = "Enter number of agents required"
     default = 3
 }
 
 variable "ssh_public_key" {
+    description = "Enter public Key Path"
     default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
+    description = "Enter dns prefix for cluster"
     default = "krishk8s"
 }
 
 variable cluster_name {
+    description = "Enter cluster name"
     default = "krishk8s"
 }
 
 variable resource_group_name {
+    description = "Enter Azure resource group name"
     default = "azure-krishk8s"
 }
 
 variable location {
+    description = "Enter Location to deploy Azure resources"
     default = "East US"
 }
+
+variable admin_username {
+    description = "Enter admin user name"
+    default = "adminuser"
+}
+
