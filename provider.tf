a4c9eb8e6373e8390662f80fe8313009ae77b302
Bhimanavarjula V krishna <bhvkrishna90@gmail.com>
2018-10-29 09:21:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
     version = "=1.5.0"
-    subscription_id = "85c574bb-8264-40fc-82b3-8204793a3d4a"
-    client_id = "61e1a624-81ed-4b8d-a1e1-1af050e6f277"
-    client_secret = "RvJsEvcYU3bAXebkG0TfS7HAD9LLJXLuSEnpqcjcJbw="
-    tenant_id = "8a6905b2-fe2d-4f79-85f3-a101cf50db14"
+    subscription_id = "85cXXXXb-XXXX-40fc-82b3-820479XXXXX"
+    client_id = "61XXXX-XXXX-4b8d-a1e1-1af05XXXXXX"
+    client_secret = "RvXXXcJbw="
+    tenant_id = "8aXXXXX-fe2d-4f79-85f3-a101XXXXXX"
 }
 
 terraform {
